ba5be6bb61757bb5dda4f756895756257fe36780
lokeshkumar56 <lokesh.coolbugs56@gmail.com>
2013-12-22 18:33:56 +0530

    lokesssssssssssehgsgrfssafd

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<HEAD>
+<TITLE>
+JavaScript to Applet Communication
+</TITLE >
+<SCRIPT LANGUAGE="JavaScript">
+ 
+</SCRIPT>
+</HEAD>
+<BODY >
+ 
+<br><font size="7" color="red">'X' man 'O' LEVEL-1 'X' man 'O'</font>
+ 
+<br><br>
+<APPLET CODE="tictac.class" NAME="tictac1" WIDTH=800 HEIGHT=600 >
+</APPLET >
+ 
+<br>
+ 
+<br><br><hr><br><br>
+ 
+</BODY >
+</html>
+<form method="post" action="tictac2.html">
+TIC TAC TOE 5 * 5:<br />
+ 
+<input type="submit" value="PLAY" /><br />
+</form>
